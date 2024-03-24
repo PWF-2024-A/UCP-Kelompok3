62119c3a0439c0c973bee39e2e4cd3e27608804c
Aisyah Fitri <aisyahafitriani05@gmail.com>
2024-03-24 15:33:04 +0700

    env, mvc, route, relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,9 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function todos(){
+        return $this->hasMany(Todo::class);
+    }
     /**
      * The attributes that should be hidden for serialization.
      *
